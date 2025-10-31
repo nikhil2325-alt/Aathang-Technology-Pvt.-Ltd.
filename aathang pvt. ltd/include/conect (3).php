4fd4e78a0526d0d11765287f2d123ab795da9c73
nikhil2325-alt <choturana2325@gmail.com>
2025-10-31 20:07:08 +0530

    Delete aathang pvt. ltd directory

--- aathang pvt. ltd/include/conect (3).php
+++
@@ -1,8 +0,0 @@
-<?php
-// Database connection
-$con = mysqli_connect('localhost', 'nnautom3_nn_admin', 'Nikhil@9175612135', 'nnautom3_nn_dashboard');
-
-if (!$con) {
-    die("Database connection failed: " . mysqli_connect_error());
-}
-?>
